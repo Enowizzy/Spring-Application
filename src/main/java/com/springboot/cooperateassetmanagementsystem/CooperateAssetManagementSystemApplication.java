02e6d4eac32ad651fc5469d91680cc9777cb24cd
Jeedie JIDE <enockyona906@gmail.com>
2022-12-18 12:48:23 +0300

    first commit

---
+++ src/main/java/com/springboot/cooperateassetmanagementsystem/CooperateAssetManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.cooperateassetmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperateAssetManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperateAssetManagementSystemApplication.class, args);
+		System.out.println("Jeedie Spring boot Developer");
+	}
+
+}
